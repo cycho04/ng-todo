9a938963e8f2beeaa68ea1014f781187c283c86f
Chris Y Cho <chrisycho04@gmail.com>
2019-01-21 07:17:26 -0800

    fetched user data. action overfetches, check video for fix

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,18 @@
 import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    console.log('fetching..')
+    await dispatch(fetchPosts());
+    console.log('fetched');
+}
+
+//fetchPosts
 // common format. es 2015 syntax. getState is not used so no need to add.
 export const fetchPosts = () => async dispatch => {
         const response = await jsonPlaceHolder.get('/posts');
         dispatch({type: 'FETCH_POSTS', payload: response.data })
     };
-
-
 
 //original format
 //
@@ -18,3 +23,12 @@ export const fetchPosts = () => async dispatch => {
 //         dispatch({type: 'FETCH_POSTS', payload: response})
 //     };
 // };
+
+
+
+
+//fetchUser
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+    dispatch({type: 'FETCH_USER', payload: response.data})
+};
